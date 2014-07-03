ca03906448621011bb086437938e417343dc70e6
Michal Tomaszewski <michalt48@gmail.com>
2014-07-03 14:27:56 +0100

    Remove the last email and render content of the iframe with JS

--- lib/letter_opener/view_helper.rb
+++ lib/letter_opener/view_helper.rb
@@ -2,12 +2,18 @@ module LetterOpener
   module ViewHelper
     def pop_email
       location = Rails.root.join("public", "letter_opener")
-      filepath = Dir.glob("#{location}/*/*.html").max_by { |f| File.mtime(f) }
-      if filepath
-        filepath = filepath.gsub(Rails.root.join("public").to_s, "")
+      absolute_filepath = Dir.glob("#{location}/*/*.html").max_by { |f| File.mtime(f) }
+      if absolute_filepath
+        filepath = absolute_filepath.gsub(Rails.root.join("public").to_s, "")
         html = <<-CONTENT
 <div id="letter_opener_container">
   <style>
+    #remove {
+      float: left;
+    }
+    h1 {
+      text-align: center;
+    }
     #letter_opener_container {
       width: 100%;
       height: 100%;
@@ -15,16 +21,30 @@ module LetterOpener
       left: 0;
       top: 0;
     }
+    #letter_opener_container iframe {
+      width: 100%;
+      height: 100%;
+      background-color: rgba(100,100,100,0.5);
+    }
   </style>
-  <a href="#" id="remove">Close</a>
+  <h1>
+    <a href="#" id="remove">Close</a>
+    You've got email!
+  </h1>
+
+  #{content_tag(:iframe, nil, src: "about:blank", id: "letter_opener_iframe")}
   <script>
     jQuery("#remove").click(function(){
       $(this).closest("div").remove();
     });
+    var doc = document.getElementById('letter_opener_iframe').contentWindow.document;
+    doc.open();
+    doc.write('#{escape_javascript(File.read(absolute_filepath))}');
+    doc.close();
   </script>
-  #{content_tag(:iframe, nil, src: filepath)}
 </div>
           CONTENT
+        `rm #{absolute_filepath}`
         html.html_safe
       end
     end
