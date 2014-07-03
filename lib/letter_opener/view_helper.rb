373ee0871951c65a60d243ab146f632a915bdf47
Michal Tomaszewski <michalt48@gmail.com>
2014-07-03 13:15:48 +0100

    fix and bump

--- lib/letter_opener/view_helper.rb
+++ lib/letter_opener/view_helper.rb
@@ -4,7 +4,7 @@ module LetterOpener
       location = Rails.root.join("public", "letter_opener")
       filepath = Dir.glob("#{location}/*/*.html").max_by { |f| File.mtime(f) }
       if filepath
-        filepath = filepath.gsub(Rails.root.to_s, "")
+        filepath = filepath.gsub(Rails.root.join("public").to_s, "")
         content_tag(:iframe, nil, src: filepath)
       end
     end
