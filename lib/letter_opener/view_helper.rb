6899f8ea9ac42b6e22e3e8310aaf8bcda03245ca
Michal Tomaszewski <michalt48@gmail.com>
2014-07-03 13:32:55 +0100

    Bump and render in a container div

--- lib/letter_opener/view_helper.rb
+++ lib/letter_opener/view_helper.rb
@@ -5,7 +5,26 @@ module LetterOpener
       filepath = Dir.glob("#{location}/*/*.html").max_by { |f| File.mtime(f) }
       if filepath
         filepath = filepath.gsub(Rails.root.join("public").to_s, "")
-        content_tag(:iframe, nil, src: filepath)
+        <<-CONTENT
+<div id="letter_opener_container">
+  <style>
+    #letter_opener_container {
+      width: 100%;
+      height: 100%;
+      position: absolute;
+      left: 0;
+      top: 0;
+    }
+  </style>
+  <a href="#" id="remove">Close</a>
+  <script>
+    jQuery("#remove").click(function(){
+      $(this).closest("div").remove();
+    });
+  </script>
+  #{content_tag(:iframe, nil, src: filepath)}
+</div>
+          CONTENT
       end
     end
   end
