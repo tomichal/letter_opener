ba3159293e2063744cceb5565dcfea3a916e10e6
Michal Tomaszewski <michalt48@gmail.com>
2014-07-03 20:08:24 +0100

    Tweak the gem to display emails in an iframe better

--- lib/letter_opener/view_helper.rb
+++ lib/letter_opener/view_helper.rb
@@ -1,48 +1,17 @@
 module LetterOpener
   module ViewHelper
-    def pop_email
-      location = Rails.root.join("public", "letter_opener")
-      absolute_filepath = Dir.glob("#{location}/*/*.html").max_by { |f| File.mtime(f) }
-      if absolute_filepath
-        filepath = absolute_filepath.gsub(Rails.root.join("public").to_s, "")
-        html = <<-CONTENT
-<div id="letter_opener_container">
-  <style>
-    #remove {
-      float: left;
-    }
-    #letter_opener_container {
-      width: 100%;
-      height: 100%;
-      position: absolute;
-      left: 0;
-      top: 0;
-    }
-    #letter_opener_container iframe {
-      width: 100%;
-      height: 100%;
-      background-color: rgba(100,100,100,0.5);
-    }
-  </style>
-  <h1>
-    <a href="#" id="remove">Close</a>
-  </h1>
-
-  #{content_tag(:iframe, nil, src: "about:blank", id: "letter_opener_iframe")}
-  <script>
-    jQuery("#remove").click(function(){
-      $(this).closest("div").remove();
-    });
-    var doc = document.getElementById('letter_opener_iframe').contentWindow.document;
-    doc.open();
-    doc.write('#{escape_javascript(File.read(absolute_filepath))}');
-    doc.close();
-  </script>
-</div>
-          CONTENT
-        `rm #{absolute_filepath}`
-        html.html_safe
-      end
+    def view_latest_message
+      template_filepath = File.join(File.dirname(__FILE__), "view_latest_message.html.erb")
+      ERB.new(File.read(template_filepath)).result(binding).html_safe
     end
+
+    private
+      def latest_message_html
+        location = Rails.root.join("tmp", "letter_opener")
+        absolute_filepath = Dir.glob("#{location}/*/*.html").max_by { |f| File.mtime(f) }
+        if absolute_filepath
+          File.read(absolute_filepath).html_safe
+        end
+      end
   end
 end
